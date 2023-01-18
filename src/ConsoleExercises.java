08df4b94fb53be4bcb168ef80e3fc10f43cf633d
Crystal <crystal.m.deleon00@gmail.com>
2023-01-17 21:03:14 -0600

    string done

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,25 +1,25 @@
 public class ConsoleExercises {
     public static void main(String[] args) {
-//        double pi = 3.14159;
-//        System.out.format("The value of pi is approximately %.2f", pi);
-        Scanner scanner = new Scanner(System.in);
-
-        int userInput = scanner.newInt();
-
-        System.out.println("Enter an integer");
-
-        System.out.println("userInput = " + userInput);
-        System.out.println("Enter three words");
-
-        String wordOne = scanner.next();
-        String wordTwo = scanner.next();
-
-        System.out.println("wordOne");
-        System.out.println("wordTwo");
-
-        Scanner getLength = new Scanner(System.in);
-        System.out.println("Enter the length of the room");
-        String length = getLength.nextLine();
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f", pi);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int userInput = scanner.newInt();
+//
+//        System.out.println("Enter an integer");
+//
+//        System.out.println("userInput = " + userInput);
+//        System.out.println("Enter three words");
+//
+//        String wordOne = scanner.next();
+//        String wordTwo = scanner.next();
+//
+//        System.out.println("wordOne");
+//        System.out.println("wordTwo");
+//
+//        Scanner getLength = new Scanner(System.in);
+//        System.out.println("Enter the length of the room");
+//        String length = getLength.nextLine();
 
 
 
---
+++ src/StringExercise.java
@@ -0,0 +1,7 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String message = "Hello from Earth!";
+        System.out.println(message);
+
+    }
+}
